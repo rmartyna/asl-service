f72ae6ae55a1787d6db9857ed8726157e2e56909
Rafał Martyna <rmartyna94@gmail.com>
2016-11-15 15:22:49 +0100

    .

--- config/run-service.sh
+++ config/run-service.sh
@@ -1,6 +1,6 @@
 #/bin/bash
 
-INSTALL_DIR=/home/rmartyna/Projects/asl-service/target
+INSTALL_DIR=/home/rmartyna/Projects/asl-service/config
 
 cd ${INSTALL_DIR}
 java -Dasl.properties=${INSTALL_DIR}/asl-service.properties -cp asl-service.jar pl.edu.agh.Main
